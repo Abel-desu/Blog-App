557ba46ac2f4f8c1d4fb1ad68ca50c87e6277615
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-06 23:07:42 +0300

    initialize files

---
+++ db/migrate/20221006184704_create_posts.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.integer :authorId
+      t.string :title
+      t.string :text
+      t.integer :commentsCounter
+      t.references :user, null: false, foreign_key: true
+      t.integer :likesCounter
+      t.belongs_to :user
+      t.has_many :comment
+      t.has_many :like
+
+      t.timestamps
+    end
+  end
+end
