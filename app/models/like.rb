c4330396f7617adab14b2fefd601686f370ede0b
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-08 12:00:46 +0300

    updates the likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, counter_cache: :likesCounter
+  belongs_to :post
+
+  after_create :update_likes_counter
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
