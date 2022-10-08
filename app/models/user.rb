6631c04aee94f6a4cc719fa557e4e5f2ec91807a
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-08 11:39:25 +0300

    fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
-  
+
   def recent_3_posts
     posts.limit(3).order(created_at: :desc)
   end
-
 end
