e723de405cf8b8c6677040fd1e2ad7cdfa2906a2
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-08 11:34:46 +0300

    add update counter for comments and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :post_id, dependent: :destroy
   has_many :likes, foreign_key: :post_id, dependent: :destroy
+
+  after_save :update_post_counter
+
+  def update_post_counter
+    author.increment!(:postsCounter)
+  end
+
+  def recent_5_comments
+    comments.order(updated_at: :desc).limit(5)
+  end
 end
