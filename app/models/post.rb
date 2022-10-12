d22cdec54183e7027938b836fcac483213162936
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-12 19:16:54 +0300

    fix error

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: :post_id, dependent: :destroy
   has_many :likes, foreign_key: :post_id, dependent: :destroy
 
-
-
   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
   validates :commentsCounter, numericality: { greater_than_or_equal_to: 0 }
   validates :likesCounter, numericality: { greater_than_or_equal_to: 0 }
-
 
   after_save :update_post_counter
 
