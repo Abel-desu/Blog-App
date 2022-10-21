45412952728b7a5815a1f613cb116fca8a0abc9f
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-21 22:11:48 +0300

    add views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
   validates :name, presence: true, allow_blank: false
-  validates :postsCounter, numericality: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_3_posts
     posts.limit(3).order(created_at: :desc)
