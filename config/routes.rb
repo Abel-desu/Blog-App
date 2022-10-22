1aa67e64ca9c2707eeef97ba7a6695455b9166c0
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-22 13:56:06 +0300

    add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-# Defines the root path route ("/")
-# root "articles#index"
-Rails.application.routes.draw do
-  root to: 'users#index'
-
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
-  end
-end
+  # Defines the root path route ("/")
+  # root "articles#index"
+  Rails.application.routes.draw do
+  
+    root to: "users#index"
+ 
+    resources :users, only: [:index, :show] do
+       resources :posts, only: [:index, :show , :new, :create] do
+         resources :comments, only: [:create , :new]
+         resources :likes, only: [:create]
+       end
+     end
+ end
+ 
