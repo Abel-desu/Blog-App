aea603af397f3a08a0cd9b08af5068111e545eea
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-10-22 14:22:25 +0300

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-  
-    root to: "users#index"
- 
-    resources :users, only: [:index, :show] do
-       resources :posts, only: [:index, :show , :new, :create] do
-         resources :comments, only: [:create , :new]
-         resources :likes, only: [:create]
-       end
-     end
- end
- 
+# Defines the root path route ("/")
+# root "articles#index"
+Rails.application.routes.draw do
+  root to: 'users#index'
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[create new]
+      resources :likes, only: [:create]
+    end
+  end
+end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /comments" do
-    it "returns http success" do
-      get "/posts/comments"
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /comments' do
+    it 'returns http success' do
+      get '/posts/comments'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
